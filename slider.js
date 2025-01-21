3e2a64cb13d098fb09f418ae61fe9d89c8b04b31
TrainHeartX <Daniel.rr93g@gmail.com>
2025-01-20 23:59:33 -0500

    0.2

--- slider.js
+++ slider.js
@@ -1,8 +1,23 @@
+// Selecciona todos los elementos con la clase.slide
 const slides = document.querySelectorAll('.slide');
+
+// Inicializa el índice de la diapositiva actual
 let currentSlide = 0;
 
-setInterval(() => {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-}, 5000);
+// Agrega la clase 'active' a la primera diapositiva inicialmente
+slides[currentSlide].classList.add('active');
+
+// Función para cambiar la diapositiva actual
+function cambiarDiapositiva() {
+  // Elimina la clase 'active' de la diapositiva actual
+  slides[currentSlide].classList.remove('active');
+
+  // Avanza al siguiente índice, o vuelve al principio si es necesario
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  // Agrega la clase 'active' a la nueva diapositiva
+  slides[currentSlide].classList.add('active');
+}
+
+// Llama a la función de cambio de diapositiva cada 5 segundos
+setInterval(cambiarDiapositiva, 5000);
