80b1b6a11b83e774364945e80c1a10403b8d16ac
TrainHeartX <Daniel.rr93g@gmail.com>
2025-01-20 22:14:00 -0500

    0.2
    continuacion de cambios

---
+++ slider.js
@@ -0,0 +1,8 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+setInterval(() => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}, 5000);
