457c64de121514df398b87453c4de759ce47a998
TrainHeartX <Daniel.rr93g@gmail.com>
2025-03-13 23:52:59 -0500

    1

--- afiliacion.js
+++ afiliacion.js
@@ -1,16 +1,17 @@
 document.getElementById("downloadPdf").addEventListener("click", async function() {
-    try {
-        const existingPdfBytes = await fetch("Afiliacion.pdf").then(res => res.arrayBuffer());
-        const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
-        const pdfBytes = await pdfDoc.save();
-
-        
-        const blob = new Blob([pdfBytes], { type: "application/pdf" });
-        const link = document.createElement("a");
-        link.href = URL.createObjectURL(blob);
-        link.download = "Afiliacion_Lleno.pdf";
-        link.click();
-    } catch (error) {
-        console.error("Error al procesar el PDF: ", error);
-    }
-});
+        try {
+            const existingPdfBytes = await fetch("Afiliacion.pdf").then(res => res.arrayBuffer());
+            const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
+            
+            pdfDoc.getForm().flatten(); // Convertir a PDF no editable
+            
+            const pdfBytes = await pdfDoc.save();
+            const blob = new Blob([pdfBytes], { type: "application/pdf" });
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.download = "Afiliacion_Lleno.pdf";
+            link.click();
+        } catch (error) {
+            console.error("Error al procesar el PDF: ", error);
+        }
+    });
