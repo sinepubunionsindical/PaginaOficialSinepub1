c60a63e52cedbd0f5cb240923bad79878acda6e1
TrainHeartX <Daniel.rr93g@gmail.com>
2025-03-13 23:32:21 -0500

    1

--- afiliacion.js
+++ afiliacion.js
@@ -3,7 +3,6 @@ document.getElementById("downloadPdf").addEventListener("click", async function(
         const existingPdfBytes = await fetch("Afiliacion.pdf").then(res => res.arrayBuffer());
         const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
         const pdfBytes = await pdfDoc.save();
-        pdfDoc.getForm().flatten(); // Convertir a PDF no editable
         
         const blob = new Blob([pdfBytes], { type: "application/pdf" });
         const link = document.createElement("a");
