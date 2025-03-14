51c970e8bc56f56a3fa09c36f2f1237d538bb574
TrainHeartX <Daniel.rr93g@gmail.com>
2025-03-13 23:26:30 -0500

    1

--- afiliacion.js
+++ afiliacion.js
@@ -1,1 +1,20 @@
 
+// Función para descargar el PDF con datos llenados y no editable
+async function downloadFilledPDF() {
+    const url = 'Afiliacion.pdf'; // Ruta del PDF base
+    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
+    const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
+    const pdfBytes = await pdfDoc.save();
+
+    // Hacer que el PDF no sea editable
+    pdfDoc.getForm().flatten();
+    
+    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'Afiliacion_Llenado.pdf';
+    link.click();
+}
+
+downloadButton.addEventListener('click', downloadFilledPDF);
+
--- slider.js
+++ slider.js
@@ -239,25 +239,6 @@ function resetInactivityTimer() {
   inactivityTimeout = setTimeout(startAutoplay, inactivityTime); // Iniciar timeout de 90 segundos
 }
 
-// Función para descargar el PDF con datos llenados y no editable
-async function downloadFilledPDF() {
-    const url = 'Afiliacion.pdf'; // Ruta del PDF base
-    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
-    const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
-    
-    // Hacer que el PDF no sea editable
-    pdfDoc.getForm().flatten();
-    
-    const pdfBytes = await pdfDoc.save();
-    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'Afiliacion_Llenado.pdf';
-    link.click();
-}
-
-downloadButton.addEventListener('click', downloadFilledPDF);
-
 // Event listeners para RESETER el timer de inactividad con CUALQUIER interacción del usuario en la PÁGINA
 document.addEventListener('mousemove', resetInactivityTimer);
 document.addEventListener('mousedown', resetInactivityTimer);
