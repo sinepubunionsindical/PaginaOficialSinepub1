1539a2ae4e6dd5779ec01c3efc0ff2cfa6cdb96b
TrainHeartX <Daniel.rr93g@gmail.com>
2025-04-20 03:00:53 -0500

    Actualización automática de URL de ngrok

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const API_KEYS = {
 };
 
 // URL de backend centralizada usando ngrok
-const BACKEND_URL = 'https://44c6-2800-484-8786-7d00-8983-3226-aa7e-4f0f.ngrok-free.app';
+const BACKEND_URL = 'https://877a-2800-484-8786-7d00-8983-3226-aa7e-4f0f.ngrok-free.app';
 
 // Modo de depuración - cambia a true para usar localhost en lugar de ngrok
 // Si hay problemas con ngrok, cambiar a true
