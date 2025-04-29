59dafdf9bf7620a35d536a68d01a790e31bb0979
Daniel Restrepo <daniel.rr93g@gmail.com>
2025-04-28 19:09:59 -0500

    Actualización automática de URL de ngrok

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const API_KEYS = {
 };
 
 // URL de backend centralizada usando ngrok
-const BACKEND_URL = 'https://d955-2800-484-8786-7d00-7c67-297a-91aa-5a20.ngrok-free.app';
+const BACKEND_URL = 'https://13e3-2800-484-8786-7d00-edc6-5af-cd5c-9843.ngrok-free.app';
 
 // Modo de depuración - cambia a true para usar localhost en lugar de ngrok
 // Si hay problemas con ngrok, cambiar a true
