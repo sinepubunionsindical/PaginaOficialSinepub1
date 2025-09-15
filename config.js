d7ca368c27dc86702b6539cabff678952de86d90
Servidor <sinepubunionsindical@gmail.com>
2025-09-14 22:36:19 -0500

    Actualización automática de URL de ngrok

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const API_KEYS = {
 };
 
 // URL de backend centralizada usando ngrok
-const BACKEND_URL = 'https://293d85f27161.ngrok-free.app';
+const BACKEND_URL = 'https://b9f1966bcf71.ngrok-free.app';
 
 // Modo de depuración - cambia a true para usar localhost en lugar de ngrok
 // Si hay problemas con ngrok, cambiar a true
