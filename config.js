e3faecf38527507a9309846f0a80f7f677332a77
Servidor <sinepubunionsindical@gmail.com>
2026-01-06 08:47:07 -0500

    Actualización automática de URL de ngrok

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const API_KEYS = {
 };
 
 // URL de backend centralizada usando ngrok
-const BACKEND_URL = 'tcp://2.tcp.ngrok.io:17619';
+const BACKEND_URL = 'tcp://0.tcp.ngrok.io:18449';
 
 // Modo de depuración - cambia a true para usar localhost en lugar de ngrok
 // Si hay problemas con ngrok, cambiar a true
